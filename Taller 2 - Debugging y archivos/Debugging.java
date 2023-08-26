8723673fd58ee4d883ec014e0aa8becf260fafa1
Tobias Llop <tobiasllop@gmail.com>
2023-08-26 12:31:45 -0300

    Update Debugging.java

--- Taller 2 - Debugging y archivos/Debugging.java
+++ Taller 2 - Debugging y archivos/Debugging.java
@@ -1,10 +1,27 @@
 package aed;
 
 class Debugging {
-
+/* ORIGINAL
+   boolean xor(boolean a, boolean b) {
+        return a || b && !(a && b);
+    } 
+*/
     boolean xor(boolean a, boolean b) {
         return (a || b) && !(a && b);
     }
+
+/* ORIGINAL
+    boolean iguales(int[] xs, int[] ys) {
+        boolean res = true;
+
+        for (int i = 0; i < xs.length; i++) {
+            if (xs[i] != ys[i]) {
+                res = false;
+            }
+        }
+        return res;
+    }
+*/
 
     boolean iguales(int[] xs, int[] ys) {
         boolean res = true;
@@ -21,6 +38,17 @@ class Debugging {
         return res;
     }
 
+/* ORIGINAL
+    boolean ordenado(int[] xs) {
+        boolean res = true;
+        for (int i = 0; i < xs.length; i++) {
+            if (xs[i] > xs [i+1]) {
+                res = false;
+            }
+        }
+        return res;
+    }
+*/  
     boolean ordenado(int[] xs) {
         boolean res = true;
         if (xs.length >= 2) {
@@ -33,6 +61,16 @@ class Debugging {
         return res;
     }
 
+/* ORIGINAL
+    int maximo(int[] xs) {
+        int res = 0;
+        for (int i = 0; i <= xs.length; i++) {
+            if (xs[i] > res) res = i;
+        }
+        return res;
+    }
+*/  
+    
     int maximo(int[] xs) {
         int res = xs[0];
         for (int i = 1; i < xs.length; i++) {
@@ -41,6 +79,20 @@ class Debugging {
         return res;
     }
 
+/* ORIGINAL
+    boolean todosPositivos(int[] xs) {
+        boolean res = false;
+        for (int x : xs) {
+            if (x > 0) {
+                res = true;
+            } else {
+                res = false;
+            }
+        }
+        return res;
+    }
+*/  
+    
     boolean todosPositivos(int[] xs) {
         boolean res = true;
         for (int i=0; i < xs.length; i++) {
